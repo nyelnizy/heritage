bbbca2b473ddeb0cb8acceed97bb7e7099958084
nyelnizy <yhiamdan@gmail.com>
2022-10-22 16:08:12 +0000

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nyelnizy/heritage
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("...And so our descendants may know whom they are. We are africans, this is our HERITAGE!")
+}
